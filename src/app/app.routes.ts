8c5137d5643d9bdf2b9651d997c03c604c0efb7c
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-05 21:40:24 +0300

    make card details for jobs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: 'job-details',
+        component: CardDetailsComponent
+    }
+];
