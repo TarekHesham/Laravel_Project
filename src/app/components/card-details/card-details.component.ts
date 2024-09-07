7a0e82c8bc9c5d69f1d254f94cd7ee0c2ceb5c24
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-07 15:52:40 +0300

    Comments form has been made

--- src/app/components/card-details/card-details.component.ts
+++ src/app/components/card-details/card-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommentsComponent } from "../comments/comments.component";
 
 @Component({
   selector: 'app-card-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommentsComponent],
   templateUrl: './card-details.component.html',
   styleUrl: './card-details.component.css'
 })
