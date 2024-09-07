8681d5c03e4751796234bfd89c61153fbd7aec35
yossef <yossefmagdy78@gmail.com>
2024-09-07 10:40:53 +0300

    add adming dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { AdminDashboardPageComponent } from './pages/admin-dashboard-page/admin-dashboard-page.component';
 
 export const routes: Routes = [
     {
@@ -13,4 +14,9 @@ export const routes: Routes = [
         component: SignupPageComponent,
         title: 'signup',
     },
+    {
+        path: 'admin-dashboard',
+        component: AdminDashboardPageComponent,
+        title: 'dashboard'
+    }
 ];
---
+++ src/app/pages/admin-dashboard-page/admin-dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-dashboard-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-dashboard-page.component.html',
+  styleUrl: './admin-dashboard-page.component.css'
+})
+export class AdminDashboardPageComponent {
+
+}
