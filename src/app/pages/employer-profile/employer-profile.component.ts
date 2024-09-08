1ca26988f970e73d07451a846de7b828eb4377a1
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-08 23:17:16 +0200

    create review applications page.

--- src/app/pages/employer-profile/employer-profile.component.ts
+++ src/app/pages/employer-profile/employer-profile.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CreateJobPostComponent } from '../create-job-post/create-job-post.component';
 import { EmployerInfoComponent } from '../../components/employer-info/employer-info.component';
 import { PostedJobsComponent } from '../../components/posted-jobs/posted-jobs.component';
+import { ReviewApplicationsComponent } from '../review-applications/review-applications.component';
 
 @Component({
   selector: 'app-employer-profile',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CreateJobPostComponent, EmployerInfoComponent, PostedJobsComponent],
+  imports: [RouterLink, RouterLinkActive, CreateJobPostComponent, EmployerInfoComponent, PostedJobsComponent, ReviewApplicationsComponent],
   templateUrl: './employer-profile.component.html',
   styleUrl: './employer-profile.component.css'
 })
---
+++ src/app/pages/review-applications/review-applications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-applications',
+  standalone: true,
+  imports: [],
+  templateUrl: './review-applications.component.html',
+  styleUrl: './review-applications.component.css'
+})
+export class ReviewApplicationsComponent {
+
+}
