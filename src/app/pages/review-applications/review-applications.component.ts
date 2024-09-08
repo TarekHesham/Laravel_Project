3bd622acaa1894a588acfffdb80033c8285bf3b1
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-08 23:46:17 +0200

    create application component.

--- src/app/pages/review-applications/review-applications.component.ts
+++ src/app/pages/review-applications/review-applications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ApplicationComponent } from '../../components/application/application.component';
 
 @Component({
   selector: 'app-review-applications',
   standalone: true,
-  imports: [],
+  imports: [ApplicationComponent],
   templateUrl: './review-applications.component.html',
   styleUrl: './review-applications.component.css'
 })
