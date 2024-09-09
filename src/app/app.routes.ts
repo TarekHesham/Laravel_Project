632293ce805171d7b897c716385d6210bc052544
Hadeer Mohamed <mhadeer169@gmail.com>
2024-09-09 22:01:31 +0300

    Dislplay Comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,7 +32,7 @@ export const routes: Routes = [
         path: 'admin-dashboard',
         component: AdminDashboardPageComponent,
         title: 'dashboard'
-    }
+    },
     {
         path: 'job-details',
         component: CardDetailsComponent
