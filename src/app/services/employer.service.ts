c28897bea79a76c70b9c4be7fb3a024fb3555e1e
Tarek Hesham <tarekghost6@gmail.com>
2024-09-09 21:59:12 +0300

    Employer service

--- src/app/services/employer.service.ts
+++ src/app/services/employer.service.ts
@@ -5,21 +5,25 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class JobService {
+export class EmployerService {
   private baseUrl = 'http://127.0.0.1:8000/api/';
 
   constructor(private http: HttpClient) { }
 
   getJobs(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/jobs`, this.getAuthHeaders());
+    return this.http.get(`${this.baseUrl}/employer/jobs`, this.getAuthHeaders());
   }
 
-  getJobById(jobId: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/jobs/${jobId}`, this.getAuthHeaders());
+  createJob(jobData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/jobs`, jobData, this.getAuthHeaders());
   }
 
-  getJobBySlug(jobSlug: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/job/${jobSlug}`, this.getAuthHeaders());
+  updateJob(jobId: number, jobData: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/jobs/${jobId}`, jobData, this.getAuthHeaders());
+  }
+
+  cancelJob(jobId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/employer/${jobId}/cancel`, this.getAuthHeaders());
   }
 
   private getAuthHeaders(): { headers: HttpHeaders } | undefined {
