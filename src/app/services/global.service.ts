f47d36ffe91e0e007ad068d2e68438b7fd77a405
Tarek Hesham <tarekghost6@gmail.com>
2024-09-09 22:41:15 +0300

    Global service

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
+  private baseUrl = 'http://127.0.0.1:8000/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  autoComplete(query: string, type: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/search/autocomplete?query=${query}&type=${type}`, this.getAuthHeaders());
+  }
+
+  addComment(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/comments`, data, this.getAuthHeaders());
+  }
+  
+  search(filters: object[]): Observable<any> {
+    // Create query string
+    const queryParams = new HttpParams();
+    Object.keys(filters).forEach(key => queryParams.set(key, filters[key]));
+
+    return this.http.get(`${this.baseUrl}/search`, {
+      headers: this.getAuthHeaders()?.headers,
+      params: queryParams
+    });
+  }
+
+  private getAuthHeaders(): { headers: HttpHeaders } | undefined {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      throw new Error('Missing token...');
+    }
+    return token ? { headers: new HttpHeaders({ 'Authorization': token }) } : undefined;
+  }
 }
