e1f2dd11b52ed8faab6c4a277b27515ae010e519
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-09 03:21:24 +0200

    testing api for showing stored jobs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JobCardComponent } from "../../components/job-card/job-card.component";
+import { JobsService } from '../../services/jobs.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,23 @@ import { JobCardComponent } from "../../components/job-card/job-card.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{ 
+
+  jobs!: any[] ;
+  token!: any;
+
+
+  constructor(private jobService:JobsService, private authService:AuthService){
+
+  }
+
+  ngOnInit() {
+    this.authService.getToken().subscribe((response:any)=>{
+      this.token = response;
+      this.jobService.getJobs(this.token).subscribe((response:any)=>{
+        this.jobs = response;
+      })
+    })
+  }
 
 }
