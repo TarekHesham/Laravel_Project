2d39557d827f544819eb9450ea0e914f3899aefa
yossef <yossefmagdy78@gmail.com>
2024-09-10 22:32:11 +0300

    redirect to home if not admin

--- src/app/components/admin-home/admin-home.component.ts
+++ src/app/components/admin-home/admin-home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { JobService } from '../../services/job.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-home',
@@ -15,11 +16,13 @@ export class AdminHomeComponent {
   posts: any;
 
 
-  constructor(private jobService: JobService, private adminService: AdminService) {}
+  constructor(private jobService: JobService, private adminService: AdminService, private router:Router) {}
 
   ngOnInit() {
     this.adminService.getComments().subscribe((response) => {
       this.comments = response.data.length;
+    }, (error) => {
+        this.router.navigate(['/']);
     });
     this.jobService.getJobs().subscribe((response) => {
       this.posts = response.length;
