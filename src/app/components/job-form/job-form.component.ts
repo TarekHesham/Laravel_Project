4ba1504e9432489980048af443e742868977c29d
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-10 05:46:01 +0200

    update multiselect component

--- src/app/components/job-form/job-form.component.ts
+++ src/app/components/job-form/job-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MultiSelectComponent } from '../multi-select/multi-select.component';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-job-form',
@@ -8,20 +9,78 @@ import { MultiSelectComponent } from '../multi-select/multi-select.component';
   templateUrl: './job-form.component.html',
   styleUrl: './job-form.component.css'
 })
-export class JobFormComponent {
+export class JobFormComponent implements OnInit {
 
-  @Input () page ='';
-  skills = [{
-    "id": 1,
-    "name": "HTML"
-  }];
-  benefits = [];
-  categories = [];
-  location = [];
-
-  autoComplete(event:any){
-    
-    console.log(event);
-    
+  constructor(private globalService: GlobalService){
   }
+
+  @Input() page = '';
+  
+  skills: any[] = [];
+  benefits: any[] = [];
+  categories: any[] = [];
+  locations: any[] = [];
+
+  ngOnInit(): void {
+    this.loadLocations();
+    this.loadSkills();
+    this.loadCategories();
+    this.loadBenefits();
+  }
+
+
+  loadSkills(): void {
+      this.globalService.skills().subscribe(
+        (response:any) => {
+          this.skills = response;  // Update the skills list
+          console.log(this.skills);
+        },
+        (error) => {
+          console.error('Error loading skills', error);
+        }
+      );
+  }
+
+  
+  loadCategories(): void {
+    this.globalService.autoComplete('all','categories').subscribe(
+      (response:any) => {
+        this.categories = response;  // Update the skills list
+        // console.log(response);
+      },
+      (error) => {
+        console.error('Error loading Categories', error);
+      }
+    );
+  }
+
+  loadBenefits(): void{
+    this.globalService.autoComplete('all','benefits').subscribe(
+      (response:any) => {
+        this.benefits = response;  // Update the skills list
+        // console.log(response);
+      },
+      (error) => {
+        console.error('Error loading Benefits', error);
+      }
+    );
+  }
+
+  loadLocations(): void {
+    this.globalService.locations().subscribe(
+      (response: any) => {
+        this.locations = response;
+        // console.log(this.locations);
+      },
+      (error) => {
+        console.error('Error loading locations', error);
+      }
+    );
+  }
+
+  // autoComplete(event:any){
+    
+  //   console.log(event);
+    
+  // }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { JobCardComponent } from "../../components/job-card/job-card.component";
-import { JobsService } from '../../services/jobs.service';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +8,15 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{ 
+export class HomeComponent { 
 
   jobs!: any[] ;
   token!: any;
 
 
-  constructor(private jobService:JobsService, private authService:AuthService){
+  constructor(){
 
   }
 
-  ngOnInit() {
-    this.authService.getToken().subscribe((response:any)=>{
-      this.token = response;
-      this.jobService.getJobs(this.token).subscribe((response:any)=>{
-        this.jobs = response;
-      })
-    })
-  }
 
 }
