504ecbdb97962cd7fc437cbb1c09884b30e057e8
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-10 07:11:31 +0200

    use data from API using authService

--- src/app/pages/employer-profile/employer-profile.component.ts
+++ src/app/pages/employer-profile/employer-profile.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CreateJobPostComponent } from '../create-job-post/create-job-post.component';
 import { EmployerInfoComponent } from '../../components/employer-info/employer-info.component';
 import { PostedJobsComponent } from '../../components/posted-jobs/posted-jobs.component';
 import { ReviewApplicationsComponent } from '../review-applications/review-applications.component';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-employer-profile',
@@ -12,6 +13,21 @@ import { ReviewApplicationsComponent } from '../review-applications/review-appli
   templateUrl: './employer-profile.component.html',
   styleUrl: './employer-profile.component.css'
 })
-export class EmployerProfileComponent {
-  selectedTab: string = 'myInfo'
+export class EmployerProfileComponent implements OnInit{
+  selectedTab: string = 'myInfo';
+  user!: any;
+  constructor(private authService:AuthService){}
+
+  ngOnInit(): void {
+    this.authService.getUser().subscribe({
+      next: (response: any) => {
+        this.user = response;
+        console.log(this.user);
+        
+      },
+      error: (error) => {
+        console.error('Error loading user', error);
+      },
+    });
+  }
 }
