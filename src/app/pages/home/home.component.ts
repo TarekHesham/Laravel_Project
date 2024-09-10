f962a1bd8ae211fd611783749909ccfd4da4c6d4
yossef <yossefmagdy78@gmail.com>
2024-09-10 20:19:50 +0300

    update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { JobCardComponent } from "../../components/job-card/job-card.component";
+import { FormGroup, ReactiveFormsModule, FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [JobCardComponent],
+  imports: [JobCardComponent, ReactiveFormsModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -14,9 +15,46 @@ export class HomeComponent {
   token!: any;
 
 
+  locations:any = [
+    {
+      name: 'egypt',
+      id: 1,
+    },
+    {
+      name: 'cairo',
+      id: 2,
+    }
+  ];
+
+
+  categories:any = [
+    {
+      name: 'engineering',
+      id: 1,
+    },
+    {
+      name: 'sales',
+      id: 2,
+    }
+  ];
+
+
+
+  selectedLocation: string = "";
+  searchForm = new FormGroup({
+    jobTitle: new FormControl(''),
+    location: new FormControl(''),
+    category: new FormControl(''),
+    experienceLevel: new FormControl(''),
+    salaryFrom: new FormControl(''),
+    salaryTo: new FormControl(''),
+    datePosted: new FormControl(''),
+  });
   constructor(){
 
   }
-
+  submit() {
+    console.log(this.searchForm.value);
+  }
 
 }
