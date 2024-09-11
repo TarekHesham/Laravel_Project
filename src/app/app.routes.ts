27c8eed5125ad926c80e58d784975c6460dca72e
Esraa_GamalH <esraa9gamal@gmail.com>
2024-09-11 04:13:28 +0200

    merged main with employer branch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
-import { AdminDashboardPageComponent } from './pages/admin-dashboard-page/admin-dashboard-page.component';
 import { CardDetailsComponent } from './components/card-details/card-details.component';
-import { CreateJobPostComponent } from './pages/create-job-post/create-job-post.component';
-import { EditJobPostComponent } from './pages/edit-job-post/edit-job-post.component';
-import { ApplyForJobComponent } from './pages/apply-for-job/apply-for-job.component';
-import { CandidateProfileComponent } from './pages/candidate-profile/candidate-profile.component';
-import { EmployerProfileComponent } from './pages/employer-profile/employer-profile.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ReviewApplicationsComponent } from './pages/review-applications/review-applications.component';
-
 import { CreateJobPostComponent } from './pages/create-job-post/create-job-post.component';
 import { EditJobPostComponent } from './pages/edit-job-post/edit-job-post.component';
 import { ApplyForJobComponent } from './pages/apply-for-job/apply-for-job.component';
@@ -57,10 +48,6 @@ export const routes: Routes = [
         title: 'Edit Post',
     },
     {
-        path: 'apply-for-job',
-        component: ApplyForJobComponent
-    },
-    {
         path: 'candidate-profile',
         component: CandidateProfileComponent
     },
@@ -73,29 +60,9 @@ export const routes: Routes = [
         path: 'applications',
         component: ReviewApplicationsComponent
     }
-
     ,
-    {
-        path: 'edit-post',
-        component: EditJobPostComponent,
-        title: 'Edit Post',
-    },
     {
         path: 'apply-for-job/:slug',
         component: ApplyForJobComponent
-    },
-    {
-        path: 'candidate-profile',
-        component: CandidateProfileComponent
-    },
-    {
-        path: 'employer-profile',
-        component: EmployerProfileComponent,
-        title: 'Employer Profile',
-    },
-    {
-        path: 'applications',
-        component: ReviewApplicationsComponent
     }
-
 ];
